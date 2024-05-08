93046567b7f2634a933e5e58f53b22111666eb07
NikhilPatel003 <ntp170405@gmail.com>
2024-05-08 11:01:19 +0530

    Creating react app

---
+++ todoLocal/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <h1 align="center" className="text-3xl font-bold underline">
+      Creating a todo app 
+    </h1>
+    </>
+  )
+}
+
+export default App
